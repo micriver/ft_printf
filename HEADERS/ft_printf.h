d8a06f576a5a17a4a27b1cbb0a42c4ee36fe6632
Michael Rivera <mirivera@e1z3r1p12.42.us.org>
2019-06-05 20:00:31 -0700

    moved header, changed makefile, changed gitignore

--- HEADERS/ft_printf.h
+++ HEADERS/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: mirivera <mirivera@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/05 10:11:59 by mirivera          #+#    #+#             */
-/*   Updated: 2019/06/05 10:13:35 by mirivera         ###   ########.fr       */
+/*   Updated: 2019/06/05 18:31:32 by mirivera         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,9 @@
 # include <string.h>
 # include <unistd.h>
 # include <fcntl.h>
-# include "libft/libft.h"
-# include "libft/get_next_line.h"
+# include "libft.h"
+# include "get_next_line.h"
+
+void     ft_printf(const char *fmt, ...);
 
 #endif
