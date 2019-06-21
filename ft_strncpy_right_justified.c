dc483b89ccee8ca096bce2d384ce0fb820dce9ba
Michael Rivera <mirivera@e1z2r3p29.42.us.org>
2019-06-21 14:34:21 -0700

    continued work on zero fill and left justify functions

--- ft_strncpy_right_justified.c
+++ ft_strncpy_right_justified.c
@@ -1,19 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strncpy.c                                       :+:      :+:    :+:   */
+/*   ft_strncpy_right_justified.c                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mirivera <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/06/19 13:58:45 by mirivera          #+#    #+#             */
-/*   Updated: 2019/06/20 15:16:45 by mirivera         ###   ########.fr       */
+/*   Created: 2019/06/21 12:21:59 by mirivera          #+#    #+#             */
+/*   Updated: 2019/06/21 14:24:54 by mirivera         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../projects/ft_printf/libft/libft.h"
+#include "libft/libft.h"
 #include <stdio.h>
 
-char	*my_strncpy(char *src, size_t n)
+char	*rj_strncpy(char *src, size_t n)
 {
 	char *dest;
 	size_t i;
@@ -23,9 +23,20 @@ char	*my_strncpy(char *src, size_t n)
 	arg_size = ft_strlen(src);
 	dest = ft_strnew(n + arg_size);
 	i = 0;
+	x = 0;
+	if (src[0] == '-')
+	{
+		dest[i] = '-';
+		src[0] = '0';
+		i++;
+	}
+	else
+	{
+		i = 0;
+		x = 0;
+	}
 	while (i < (n - arg_size))
 		dest[i++] = '0';
-	x = 0;
 	while (src[x])
 		dest[i++] = src[x++];
 	printf("The width given is: %zu\n", n);
@@ -35,11 +46,11 @@ char	*my_strncpy(char *src, size_t n)
 
 int		main()
 {
-	char src[] = "7777";
+	char src[] = "-7";
 	//char dest[] = "";
 	printf("argument is: %s\n", src);
 //	printf("printed string is: %s\n", dest);
-	printf("%s\n", my_strncpy(src, 10));
+	printf("%s\n", rj_strncpy(src, 5));
 	//printf("argument is now: %s\n", src);
 	//printf("printed string is now: %s\n", dest);
 	return (0);
--- left_justfied_strncpy.c
+++ left_justfied_strncpy.c
@@ -1,44 +1,46 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strncpy.c                                       :+:      :+:    :+:   */
+/*   left_justfied_strncpy.c                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mirivera <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/06/19 13:58:45 by mirivera          #+#    #+#             */
-/*   Updated: 2019/06/20 14:06:22 by mirivera         ###   ########.fr       */
+/*   Created: 2019/06/21 12:23:45 by mirivera          #+#    #+#             */
+/*   Updated: 2019/06/21 13:31:29 by mirivera         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../projects/ft_printf/libft/libft.h"
+#include "libft/libft.h"
 #include <stdio.h>
 
-char	*ft_strncpy(char *dest, const char *src, size_t n)
+char	*lj_strncpy(char *src, size_t n)
 {
+	char *dest;
 	size_t i;
+	size_t x;
+	int arg_size;
 
+	arg_size = ft_strlen(src);
+	dest = ft_strnew(n + arg_size);
+	x = 0;
 	i = 0;
-	while (src[i] != '\0' && i < n)
-	{
-		dest[i] = src[i];
-		++i;
-	}
+	while (src[x])
+		dest[i++] = src[x++];
 	while (i < n)
-	{
-		dest[i] = 'x';
-		i++;
-	}
+		dest[i++] = '0';
+	printf("The width given is: %zu\n", n);
+	printf("%zu\n", ft_strlen(dest));
 	return (dest);
 }
 
 int		main()
 {
-	char src[] = "7";
-	char dest[] = "";
+	char src[] = "821000";
+	//char dest[] = "";
 	printf("argument is: %s\n", src);
 //	printf("printed string is: %s\n", dest);
-	ft_strncpy(dest, src, 5);
+	printf("%s\n", lj_strncpy(src, 10));
 	//printf("argument is now: %s\n", src);
-	printf("printed string is now: %s\n", dest);
+	//printf("printed string is now: %s\n", dest);
 	return (0);
 }
