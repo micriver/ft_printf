fe11b80bc5a6c5266f3469cae74c95b9ba36f053
Michael Rivera <mirivera@e1z3r3p16.42.us.org>
2019-06-26 14:26:28 -0700

    added macros for bitfield fields

--- bitfields.c
+++ bitfields.c
@@ -8,6 +8,20 @@
 #include <unistd.h>
 
 #define CHECK_BIT(var,pos) ((var >> pos) & 1) 
+#define SET_BIT(var,pos) (var |= 1 << pos) 
+#define TOGGLE_BIT(var,pos) (var ^= (1 << pos)) 
+
+#define ZERO_F (0)
+#define MINUS_F (1)
+#define PLUS_F (2)
+#define SHARP_F (3)
+#define SPACE_F (4)
+
+//#define ZERO_F (5)
+//#define ZERO_F (6)
+//#define ZERO_F (7)
+/*think about changing POSITION TO FLAG!!!!*/
+
 
 struct 
 {
@@ -23,8 +37,8 @@ struct
 
 void	print_bits(unsigned char octet)
 {
-	d1.member2 |= 1 << 0;
-	d1.member2 |= 1 << 4;
+	//d1.member2 |= 1 << 0;
+	//d1.member2 |= 1 << 4;
 	int i;
 	unsigned char bit;
 
@@ -42,14 +56,18 @@ int		main()
 	int bit;
 	int bit2;
 // there will be 8 bytes taken since each int is 4 bytes and theres two members
-	//printf("size of regular structure = %lu\n", sizeof(d1));
-	//printf("size of structure with bit field = %lu\n", sizeof(d2));
+	printf("size of regular structure = %lu\n", sizeof(d1));
+	printf("size of structure with bit field = %lu\n", sizeof(d2));
 	printf("Setting bits:\n");
-	printf("d1.member2 |= 1 << 4\n");
-	printf("d1.member2 |= 1 << 0");
-	printf("\n\n");
-	d1.member2 |= 1 << 4;
-	d1.member2 |= 1 << 0;
+//	printf("d1.member2 |= 1 << 4\n");
+//	printf("d1.member2 |= 1 << 0");
+//	printf("\n\n");
+	SET_BIT(d1.member2, ZERO_F);
+	SET_BIT(d1.member2, SPACE_F);
+	//SET_BIT(d1.member2, 2);
+	//SET_BIT(d1.member2, 3);
+	//d1.member2 |= 1 << 4;
+	//d1.member2 |= 1 << 0;
 	printf("d1.member2 = ");
 	printf("\n");
 	print_bits(d1.member2);
@@ -84,18 +102,17 @@ int		main()
 	d1.member2 &= ~(1 << 0);
 	printf("\n");
 
-
-
-
 	printf("Toggle (0)th bit:\n");
 	printf("BEFORE:\n");
 	printf("\n");
-	d1.member2 ^= (1 << 0);
+	TOGGLE_BIT(d1.member2, ZERO_F);
+	//d1.member2 ^= (1 << 0);
 	print_bits(d1.member2);
 	printf("\n");
 	printf("AFTER:\n");
 	printf("\n");
-	d1.member2 ^= (1 << 0);
+	TOGGLE_BIT(d1.member2, ZERO_F);
+	//d1.member2 ^= (1 << 0);
 	print_bits(d1.member2);
 	printf("\n");
 	printf("\n");
