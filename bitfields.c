93a608e786833aee7ed52f7b349f6ae28959b5a3
Michael Rivera <mirivera@e1z3r3p16.42.us.org>
2019-06-25 19:48:14 -0700

    modified bitfields file

--- bitfields.c
+++ bitfields.c
@@ -38,6 +38,8 @@ void	print_bits(unsigned char octet)
 
 int		main()
 {
+	int bit;
+	int bit2;
 // there will be 8 bytes taken since each int is 4 bytes and theres two members
 	//printf("size of regular structure = %lu\n", sizeof(d1));
 	//printf("size of structure with bit field = %lu\n", sizeof(d2));
@@ -48,13 +50,45 @@ int		main()
 	d1.member2 |= 1 << 4;
 	d1.member2 |= 1 << 0;
 	printf("d1.member2 = ");
+	printf("\n");
 	print_bits(d1.member2);
 	printf("\n\n");
+
+	printf("Checking bit state...\n");
+	bit = (d1.member2 >> 4) & 1;
+	bit2 = (d1.member2 >> 8) & 1;
+	printf("The bit state for the 4th index is: %d\n", bit);
+	printf("The bit state for the 8th index is: %d\n", bit2);
+	printf("\n");
+	print_bits(d1.member2);
+	printf("\n");
+
+
 	printf("Clearing bits:\n");
 	d1.member2 &= ~(1 << 0);
 	printf("d1.member2 = ");
+	printf("\n");
 	print_bits(d1.member2);
 	printf("\n");
+	d1.member2 &= ~(1 << 0);
+	printf("\n");
+
+
+
+	printf("Toggle (0)th bit:\n");
+	printf("BEFORE:\n");
+	printf("\n");
+	d1.member2 ^= (1 << 0);
+	print_bits(d1.member2);
+	printf("\n");
+	printf("AFTER:\n");
+	printf("\n");
+	d1.member2 ^= (1 << 0);
+	print_bits(d1.member2);
+	printf("\n");
+
+
+
 //	&= ~
 	return (0);
 }
