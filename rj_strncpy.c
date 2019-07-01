57b9ede9ac53060481984f70df7286a9cdfe51f6
Michael Rivera <mirivera@e1z3r3p16.42.us.org>
2019-07-01 12:59:12 -0700

    created leading zero negative sign handling function

--- rj_strncpy.c
+++ rj_strncpy.c
@@ -6,7 +6,7 @@
 /*   By: mirivera <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/27 13:14:55 by mirivera          #+#    #+#             */
-/*   Updated: 2019/06/27 14:17:29 by mirivera         ###   ########.fr       */
+/*   Updated: 2019/07/01 12:58:35 by mirivera         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,11 +28,30 @@
 
 struct arguments
 {
-	int arg1 : 16; //make sure that you set the bit depth to only the amount of bits you actually need!!! eg, the # of flags/modifiers you'll be using
+	int arg1 : 16; //make sure that you set the bit width to only the amount of bits you actually need!!! eg, the # of flags/modifiers you'll be using
 	int arg2 : 16;
 } args;
 
-char	*rj_strncpy(char *src, size_t n)
+char	*lead_zero_negsign(char *dest, char *src, int size)
+{
+	int y;
+	int j;
+
+	y = 0;
+	j = 0;
+	while (src[y])
+	{
+		if (src[0] == '-' && (CHECK_BIT(args.arg1, ZERO_F)))
+		{
+			dest = prefixchar('-', dest);
+			src[0] = '0';
+		}
+		dest[size++] = src[y++];
+	}
+	return (dest);
+}
+
+char	*rj_strncpy(char *src, size_t width)
 {
 	char *dest;
 	size_t i;
@@ -40,37 +59,32 @@ char	*rj_strncpy(char *src, size_t n)
 	int arg_size;
 
 	arg_size = ft_strlen(src);
-	dest = ft_strnew(n + arg_size);
+	dest = ft_strnew(width + arg_size);
 	i = 0;
 	x = 0;
-	while (i < (n - arg_size))
+	while (i < (width - arg_size))
 	{
 		dest[i] = ' ';
 		(CHECK_BIT(args.arg1, ZERO_F)) ? (dest[i++] = '0') : (dest[i++]);
 	}
-	while (src[x])
-	{
-		if (src[0] == '-' && (CHECK_BIT(args.arg1, ZERO_F)))
-		{
-			dest = prefixchar('-', dest);
-			src[0] = '0';
-		}
-		dest[i++] = src[x++];
-	}
+	dest = lead_zero_negsign(dest, src, (width - arg_size));
 	return (dest);
 }
 
 int		main()
 {
-	//SET_BIT(args.arg1, ZERO_F);
-	char src[] = "-7";
-	int x = -7;
+//	SET_BIT(args.arg1, ZERO_F);
+	char src[] = "8899";
+	int x = 8899;
 	//char dest[] = "";
 	//printf("argument is: %s\n", src);
 //	printf("printed string is: %s\n", dest);
 	//printf("%s\n", rj_strncpy(src, 5));
 	printf("ft_printf:%s\n", rj_strncpy(src, 10));
-	printf("printf   :%10d\n", x);
+	if (CHECK_BIT(args.arg1, ZERO_F))
+		printf("printf   :%010d\n", x);
+	else
+		printf("printf   :%10d\n", x);
 	//printf("argument is now: %s\n", src);
 	//printf("printed string is now: %s\n", dest);
 	return (0);
