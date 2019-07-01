964745c4109c5053b9fe195a5615fc765e376a24
Michael Rivera <mirivera@e1z3r3p16.42.us.org>
2019-07-01 14:21:48 -0700

    further work on getting the width to behave correctly

--- HEADERS/ft_printf.h
+++ HEADERS/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: mirivera <mirivera@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/05 10:11:59 by mirivera          #+#    #+#             */
-/*   Updated: 2019/06/27 13:54:16 by mirivera         ###   ########.fr       */
+/*   Updated: 2019/07/01 13:32:45 by mirivera         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,23 @@
 # include <fcntl.h>
 # include "libft.h"
 # include "get_next_line.h"
+
+#define CHECK_BIT(var,pos) ((var >> pos) & 1) 
+#define SET_BIT(var,pos) (var |= 1 << pos) 
+#define TOGGLE_BIT(var,pos) (var ^= (1 << pos)) 
+
+#define ZERO_F (0)
+#define MINUS_F (1)
+#define PLUS_F (2)
+#define SHARP_F (3)
+#define INVP_F (4)
+#define SPACE (5)
+
+struct arguments
+{
+	int arg1 : 16; //make sure that you set the bit width to only the amount of bits you actually need!!! eg, the # of flags/modifiers you'll be using
+	int arg2 : 16;
+} args;
 
 void	ft_printf(const char *fmt, ...);
 char	*prefixchar(char prefix, const char *str);
--- rj_strncpy.c
+++ rj_strncpy.c
@@ -6,31 +6,12 @@
 /*   By: mirivera <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/27 13:14:55 by mirivera          #+#    #+#             */
-/*   Updated: 2019/07/01 12:58:35 by mirivera         ###   ########.fr       */
+/*   Updated: 2019/07/01 14:20:59 by mirivera         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft/libft.h"
+#include "./HEADERS/ft_printf.h"
 #include <stdio.h>
-#include <string.h>
-#include <unistd.h>
-
-#define CHECK_BIT(var,pos) ((var >> pos) & 1) 
-#define SET_BIT(var,pos) (var |= 1 << pos) 
-#define TOGGLE_BIT(var,pos) (var ^= (1 << pos)) 
-
-#define ZERO_F (0)
-#define MINUS_F (1)
-#define PLUS_F (2)
-#define SHARP_F (3)
-#define INVP_F (4)
-#define SPACE (' ')
-
-struct arguments
-{
-	int arg1 : 16; //make sure that you set the bit width to only the amount of bits you actually need!!! eg, the # of flags/modifiers you'll be using
-	int arg2 : 16;
-} args;
 
 char	*lead_zero_negsign(char *dest, char *src, int size)
 {
@@ -45,7 +26,11 @@ char	*lead_zero_negsign(char *dest, char *src, int size)
 		{
 			dest = prefixchar('-', dest);
 			src[0] = '0';
+			//size += 1;
+			//src += 1;
 		}
+		else if (ft_atoi(src) >= 0 && CHECK_BIT(args.arg1, PLUS_F))
+			dest = prefixchar('+', dest);
 		dest[size++] = src[y++];
 	}
 	return (dest);
@@ -62,29 +47,36 @@ char	*rj_strncpy(char *src, size_t width)
 	dest = ft_strnew(width + arg_size);
 	i = 0;
 	x = 0;
-	while (i < (width - arg_size))
+	if (width > (size_t)arg_size)
 	{
-		dest[i] = ' ';
-		(CHECK_BIT(args.arg1, ZERO_F)) ? (dest[i++] = '0') : (dest[i++]);
+		while (i < (width - arg_size))
+		{
+			dest[i] = ' ';
+			(CHECK_BIT(args.arg1, ZERO_F)) ? (dest[i++] = '0') : (dest[i++]);
+		}
+		dest = lead_zero_negsign(dest, src, (width - arg_size));
 	}
-	dest = lead_zero_negsign(dest, src, (width - arg_size));
+	while (src[i])
+		dest[x++] = src[i++];
 	return (dest);
 }
 
 int		main()
 {
-//	SET_BIT(args.arg1, ZERO_F);
-	char src[] = "8899";
-	int x = 8899;
+	int width = 10;
+	SET_BIT(args.arg1, ZERO_F);
+	//SET_BIT(args.arg1, PLUS_F);
+	char src[] = "-8899";
+	int x = -8899;
 	//char dest[] = "";
 	//printf("argument is: %s\n", src);
 //	printf("printed string is: %s\n", dest);
 	//printf("%s\n", rj_strncpy(src, 5));
-	printf("ft_printf:%s\n", rj_strncpy(src, 10));
+	printf("ft_printf:%s\n", rj_strncpy(src, width));
 	if (CHECK_BIT(args.arg1, ZERO_F))
-		printf("printf   :%010d\n", x);
+		printf("printf   :%0*d\n", width, x);
 	else
-		printf("printf   :%10d\n", x);
+		printf("printf   :%*d\n", width, x);
 	//printf("argument is now: %s\n", src);
 	//printf("printed string is now: %s\n", dest);
 	return (0);
