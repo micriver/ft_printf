4be57604ed1593f6dc5e0908fd23b60a911173ff
Michael Rivera <mirivera@student.42.us.org>
2019-07-30 14:06:30 -0700

    .

--- srcs/u_specifier.c
+++ srcs/u_specifier.c
@@ -6,11 +6,12 @@
 /*   By: mirivera <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/29 15:15:22 by mirivera          #+#    #+#             */
-/*   Updated: 2019/07/29 15:53:12 by mirivera         ###   ########.fr       */
+/*   Updated: 2019/07/29 19:48:18 by mirivera         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../headers/ft_printf.h"
+#include <stdint.h>
 
 int64_t		u_return_value(va_list args)
 {
@@ -18,11 +19,11 @@ int64_t		u_return_value(va_list args)
 
 	u = 0;
 	if (CHECK_BIT(arg.flgmods, SHOINT))
-		u = ((unsigned short int)va_arg(args, int64_t));
-	//else if (CHECK_BIT(arg.flgmods, LONGINT))
-	//	u = ((long int)va_arg(args, int64_t));
-	//else if (CHECK_BIT(arg.flgmods, LNGLNG))
-	//	u = ((long long)va_arg(args, int64_t));
+		u = ((unsigned short int)va_arg(args, uint64_t));
+	else if (CHECK_BIT(arg.flgmods, LONGINT))
+		u = ((unsigned long int)va_arg(args, uint64_t));
+	else if (CHECK_BIT(arg.flgmods, LNGLNG))
+		u = ((unsigned long long int)va_arg(args, uint64_t));
 	else
 		u = ((unsigned int)va_arg(args, int));
 	return (u);
@@ -32,14 +33,14 @@ char	*u_specifier(int64_t u)
 {
 	char *result;
 
-	//if (CHECK_BIT(arg.flgmods, SHOINT))
-	//	result = ft_itoa(u);
-	//else if (CHECK_BIT(arg.flgmods, LONGINT))
-	//	result = ft_itoa((long int)va_arg(args, int64_t));
-	//else if (CHECK_BIT(arg.flgmods, LNGLNG))
-	//	result = ft_long_itoa(u);
-	result = ft_long_itoa(u);
-		//result = NULL;
+	if (CHECK_BIT(arg.flgmods, SHOINT))
+		result = ft_itoa(u);
+	else if (CHECK_BIT(arg.flgmods, LONGINT))
+		result = ft_itoa(u);
+	else if (CHECK_BIT(arg.flgmods, LNGLNG))
+		result = ft_long_itoa(u);
+	else
+		result = NULL;
 	return (result);
 }
 
