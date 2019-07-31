262ee4fac0e1176485ab3dd3a5bb962c2c5e55f2
Michael Rivera <mirivera@student.42.us.org>
2019-07-31 16:41:23 -0700

    forgot to mention I added uitoa, removed .sw files and I also consolidated a TON of lines in functions

--- libft/preprendchar.c
+++ libft/preprendchar.c
@@ -6,23 +6,39 @@
 /*   By: mirivera <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/31 16:24:41 by mirivera          #+#    #+#             */
-/*   Updated: 2019/07/31 16:28:37 by mirivera         ###   ########.fr       */
+/*   Updated: 2019/07/31 16:38:38 by mirivera         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*prependchar(char *pstr, char *str)
+char	*prependchar(char c, char *str)
 {
 	unsigned int i;
 	unsigned int j;
+	char *pstr;
 
+	pstr = ft_strnew(ft_strlen(str + 1));
 	i = 0;
-	while (pstr[i] != '\0')
-		i++;
 	j = 0;
-	while (str[j] != '\0')
-		pstr[i++] = str[j++];
+	pstr[i] = c;
+	while (pstr[i])
+		pstr[++i] = str[j++];
 	pstr[i] = '\0';
 	return (pstr);
 }
+
+//char	*prependchar(char *pstr, char *str)
+//{
+//	unsigned int i;
+//	unsigned int j;
+//
+//	i = 0;
+//	while (pstr[i] != '\0')
+//		i++;
+//	j = 0;
+//	while (str[j] != '\0')
+//		pstr[i++] = str[j++];
+//	pstr[i] = '\0';
+//	return (pstr);
+//}
