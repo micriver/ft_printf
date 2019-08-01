9ee3de85362589e8e5e6c15ccf0f95258b3d059e
Michael Rivera <mirivera@student.42.us.org>
2019-08-01 15:57:45 -0700

    cleaned up conversion file

--- srcs/conversion.c
+++ srcs/conversion.c
@@ -6,7 +6,7 @@
 /*   By: mirivera <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/02 14:01:55 by mirivera          #+#    #+#             */
-/*   Updated: 2019/08/01 14:37:41 by mirivera         ###   ########.fr       */
+/*   Updated: 2019/08/01 15:57:27 by mirivera         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,22 +17,13 @@
 char	*conversion(int *i, char *str, va_list args)
 {
 	char *result;
-	//int64_t di;
 
 	master_pars(str, i);
-	//majority of the work has to happen below here now
-	
 	//place a while loop here where you loop through n
-	
 	//and a list of specifiers in a string
 	//while loop and dispatch table here
 	if (str[*i] == 'd' || str[*i] == 'i' )
-	{
-		//int64_t di; //this needs to be able to hold at least 64 bits
-					//for whatever value comes back from di_return value
 		result = di_ret_val(args);
-		//result = di_specifier(di);
-	}
 	else if (str[*i] == 'u')
 	{
 		if (CHECK_BIT(arg.flgmods, LNGLNG))
