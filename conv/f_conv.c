6d2610e16249741986eb6f9178e9e2c679c61b63
Michael Rivera <mirivera@student.42.us.org>
2019-08-29 10:15:14 -0700

    ready to norme f_conv but have to switch computers

--- conv/f_conv.c
+++ conv/f_conv.c
@@ -6,51 +6,21 @@
 /*   By: mirivera <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/25 14:21:37 by mirivera          #+#    #+#             */
-/*   Updated: 2019/08/28 20:37:23 by mirivera         ###   ########.fr       */
+/*   Updated: 2019/08/29 10:14:47 by mirivera         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../headers/ft_printf.h"
-#include <stdio.h>
-
-//unsigned long long	ft_power(int base, unsigned int exponent)
-//{
-//	unsigned long long result;
-//
-//	result = 1;
-//	if (exponent == 0)
-//		return (1);
-//	while (exponent != 0)
-//	{
-//		result *= base;
-//		--exponent;
-//	}
-//	return (result);
-//}
-
-//double		round(double value)
-//{
-//	double magic;
-//	
-//	//instead of .45, the rounded values
-//	//matched with regular printf
-//	//when it was changed to 0.25
-//	magic = 0.25;
-//	if (arg.precision != 0)
-//	{
-//		magic /= (ft_power(10, arg.precision));
-//		return (value += magic);
-//	}
-//	else
-//		return (value += magic);
-//}
 
 char	*roundup(char *str)
 {
 	int i;
 	char number;
 
-	i = arg.precision;
+	if (CHECK_BIT(arg.flgmods, LONEDEC) || arg.precision == 0)
+		i = 3;
+	else
+		i = arg.precision;
 	number = str[i - 1];
 	if (number > '5' && number != '9')
 		str[i - 1] = number + 1;
@@ -82,7 +52,52 @@ long double		decconv(long double dec)
 	return (dec);
 }
 
+void	lonedec(char *whlnum, char *decnum)
+{
+	if (ft_strcmp(whlnum, "0") == 0)
+	{
+		ft_putstr(whlnum);
+		ft_putchar('.');
+		while (arg.precision-- > 0)
+			ft_putchar('0');
+	}
+	else
+	{
+		char number;
 
+		number = whlnum[ft_strlen(whlnum) - 1];
+		if (decnum[0] >= '5' && number != 9)
+			whlnum[ft_strlen(whlnum) - 1] = number + 1;
+		if (decnum[0] >= '5' && number == '9')
+		{
+			whlnum[ft_strlen(whlnum) - 1] = '0';
+			whlnum[ft_strlen(whlnum) - 2] = '1';
+		}
+		ft_putstr(whlnum);
+	}
+}
+
+void	preczero(char *whlnum, char *decnum)
+{
+	char number;
+
+	number = whlnum[ft_strlen(whlnum) - 1];
+	if (decnum[0] >= '5' && number != 9)
+		whlnum[ft_strlen(whlnum) - 1] = number + 1;
+	if (decnum[0] >= '5' && number == '9')
+	{
+		whlnum[ft_strlen(whlnum) - 1] = '0';
+		whlnum[ft_strlen(whlnum) - 2] = '1';
+	}
+	ft_putstr(whlnum);
+}
+
+void	precpresent(char *whlnum, char *decnum)
+{
+		ft_putstr(whlnum);
+		ft_putchar('.');
+		ft_putstr(decnum);
+}
 
 void	f_conv(va_list args)
 {
@@ -96,64 +111,17 @@ void	f_conv(va_list args)
 	decnum = NULL;
 	ipart = (int)num;
 	whlnum = ft_llitoa(ipart);
-	//if just a decimal point appears
-	if (ft_strcmp(whlnum, "0") == 0)
-	{
-		ft_putstr(whlnum);
-		ft_putchar('.');
-		while (arg.precision-- > 0)
-			ft_putchar('0');
-	}
-	else if (CHECK_BIT(arg.flgmods, LONEDEC))
-	{
-		char number;
-
-		number = whlnum[ft_strlen(whlnum) - 1];
-		fpart = num - ipart;	
-		fpart = decconv(fpart);
-		ipart = (long long int)fpart;
-		decnum = ft_llitoa(ipart);
-		if (decnum[0] >= '5' && number != 9)
-			whlnum[ft_strlen(whlnum) - 1] = number + 1;
-		if (decnum[0] >= '5' && number == '9')
-		{
-			whlnum[ft_strlen(whlnum) - 1] = '0';
-			whlnum[ft_strlen(whlnum) - 2] = '1';
-		}
-		ft_putstr(whlnum);
-	}
-	//if the precision is explicitly zero, no decimal char appears
+	fpart = num - ipart;	
+	fpart = decconv(fpart);
+	ipart = (long long int)fpart;
+	decnum = ft_llitoa(ipart);
+	decnum = roundup(decnum);
+	if (CHECK_BIT(arg.flgmods, LONEDEC))
+		lonedec(whlnum, decnum);
 	else if (arg.precision == 0)
-	{
-		char number;
-
-		number = whlnum[ft_strlen(whlnum) - 1];
-		fpart = num - ipart;	
-		fpart = decconv(fpart);
-		ipart = (long long int)fpart;
-		decnum = ft_llitoa(ipart);
-		if (decnum[0] >= '5' && number != 9)
-			whlnum[ft_strlen(whlnum) - 1] = number + 1;
-		if (decnum[0] >= '5' && number == '9')
-		{
-			whlnum[ft_strlen(whlnum) - 1] = '0';
-			whlnum[ft_strlen(whlnum) - 2] = '1';
-		}
-		ft_putstr(whlnum);
-	}
-	//if there is a precision
+		preczero(whlnum, decnum);
 	else if (arg.precision > 0)
-	{
-			
-		fpart = num - ipart;	
-		fpart = decconv(fpart);
-		ipart = (long long int)fpart;
-		decnum = ft_llitoa(ipart);
-		decnum = roundup(decnum);
-		ft_putstr(whlnum);
-		ft_putchar('.');
-		ft_putstr(decnum);
-	}
+		precpresent(whlnum, decnum);
 	free(whlnum);
 	free(decnum);
 }
