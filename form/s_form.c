e06e22b65ed64bc6e6e9c7408c43a2e98aaea61c
Michael Rivera <mirivera@student.42.us.org>
2019-09-07 16:01:15 -0700

    pretty good progress still on s

--- form/s_form.c
+++ form/s_form.c
@@ -6,7 +6,7 @@
 /*   By: mirivera <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/29 23:55:25 by mirivera          #+#    #+#             */
-/*   Updated: 2019/09/07 14:47:29 by mirivera         ###   ########.fr       */
+/*   Updated: 2019/09/07 16:00:12 by mirivera         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,16 +39,6 @@ char		*s_wbuild(char *temp, char *origstr)
 
 	i = 0;
 	formstr = ft_strnew(arg.width);
-	if (CHECK_BIT(arg.flgmods, LONEDEC))
-	{
-		temp2 = temp;
-		temp2 = "";
-		free(temp);
-		while (i < (WIDTH - (int)ft_strlen(temp2)))
-			formstr[i++] = ' ';
-		temp = ft_strjoin(temp2, formstr);
-		return (temp);
-	}
 	if (ft_strcmp(origstr, "(null)") == 0)
 		diff = arg.width - (int)ft_strlen(temp);
 	else if ((arg.width >= (int)ft_strlen(temp)) || (ft_strcmp(origstr, "(null)") == 0))
@@ -72,8 +62,6 @@ char		*s_wbuild(char *temp, char *origstr)
 		}
 		else
 		{
-			//if (CHECK_BIT(arg.flgmods, LONEDEC))
-			//	free(temp);
 			temp = ft_strjoin(formstr, temp2);
 			free(temp2);
 		}
@@ -82,19 +70,55 @@ char		*s_wbuild(char *temp, char *origstr)
 	return (temp);
 }
 
+char		*s_lonedec(void)
+{
+	char	*formstr;
+	int		i;
+
+	i = 0;
+	if (WIDTH)
+	{
+		formstr = ft_strnew(WIDTH);
+		while (i < WIDTH)
+			formstr[i++] = ' ';
+		formstr[i] = '\0';
+		return (formstr);
+	}
+	else
+		return ("");
+}
+
 void		s_form(char *origstr)
 {
 	char	*temp;
 	char	*temp2;
 
-	temp = ft_strdup(origstr);
-	if (arg.precision)
-		temp = s_pbuild(temp, origstr);
-	if (arg.width)
+	if ((!PREC) && (!WIDTH) && (!LONEDEC_F))
+		arg.char_count += ft_intputstr(origstr);
+	else if ((PREC) && (!LONEDEC_F))
 	{
-		temp2 = s_wbuild(temp, origstr);
-		prfree(temp2);
+		temp = ft_strdup(origstr);
+		temp = s_pbuild(temp, origstr);
+		if (!WIDTH)
+			prfree(temp);
 	}
-	else
+	if ((WIDTH) && (!LONEDEC_F))
+	{
+		if (PREC)
+		{
+			temp2 = s_wbuild(temp, origstr);
+			prfree(temp2);
+		}
+		else
+		{
+			temp = ft_strdup(origstr);
+			temp2 = s_wbuild(temp, origstr);
+			prfree(temp2);
+		}
+	}
+	else if ((LONEDEC_F) && (PREC == 0))
+	{
+		temp = s_lonedec();
 		prfree(temp);
+	}
 }
